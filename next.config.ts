8c8beeb93170aa4172b3da4f5aaa9b24e084a9b3
avtanos <avtanos@gmail.com>
2025-09-09 18:54:11 +0600

    Исправление GitHub Actions workflow - объединение build и deploy

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: "/ubk-application",
   assetPrefix: "/ubk-application",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
