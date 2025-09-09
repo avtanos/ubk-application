f30ea8b7379a0fc793a1b0548c194b803b5bd390
avtanos <avtanos@gmail.com>
2025-09-09 18:44:36 +0600

    Исправление стилей для GitHub Pages - добавлен basePath и assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/ubk-application",
+  assetPrefix: "/ubk-application",
   images: {
     unoptimized: true,
   },
